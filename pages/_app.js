fcde202989d292da0df5bfbda4d7ef8beaab6fc1
perSimone <40167470+perSimone@users.noreply.github.com>
2022-02-27 19:21:45 +0100

    tosti app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Link from "next/link";
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+  return (
+    <>
+    <nav className="header">
+      <div>
+        <Link href="/">
+          <a>Bring me home please</a>
+        </Link>
+      </div>
+    </nav>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )}
 
 export default MyApp
